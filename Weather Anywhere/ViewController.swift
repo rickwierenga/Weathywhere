0497745b227dfddfc2afe359d0fd404e6c3695fe
rickwierenga <rick_wierenga@icloud.com>
2019-02-12 10:18:16 +0100

    Initial Commit

---
+++ Weather Anywhere/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather Anywhere
+//
+//  Created by Rick Wierenga on 12/02/2019.
+//  Copyright © 2019 Rick Wierenga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
